b50af6dd60212e5d4bce56ccec2d2f7711c2c35f
Clinton Halpin <chalpin@clearstreet.io>
2018-11-07 20:38:18 -0500

    Tweak

--- README.md
+++ README.md
@@ -3,6 +3,10 @@ Basic Ansible Setup up to spin up docker ready machines
 
 get started
 ```
-./init
+./init.sh
 ansible-playbook roles/provision.yml --e "target_group=local"
 ```
+
+notes
+- All `init.sh` does is copy sample files over and renames them
+- You'll need to change your target_groups to something that actually exists in your hosts ie. production
--- init.sh
+++ init.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-echo '[☠️] Setting up Ansible'
-echo '[..] copy config'
+echo '☠️ Setting up Ansible'
+echo '  -> copy config'
 cp ansible.cfg.sample ansible.cfg
 cp hosts.sample hosts
 cp vars.yml.sample vars.yml
-rm -rf ansible.cfg.sample hosts.sample vars.sample.yml
 
-echo '[..] maybe install requirements'
+mv ansible.cfg.sample hosts.sample vars.sample.yml
+
+echo '  -> maybe install requirements'
 ansible-galaxy install -r requirements.yml --force
 
-echo '[✅] Setup Complete'
+echo '  -> Setup Complete'
